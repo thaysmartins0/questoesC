a0d8c2e8b5157c4e25fffd2420a42d7932861d97
thays <thaysfmcandido14@gmail.com>
2026-04-02 21:53:51 -0300

    Update questao6.c

---
+++ questao6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+float raio;
+float area;
+    printf("informe a medida do raio da circunferencia: ");
+    scanf("%f", &raio);
+    area= raio*raio*3.14;
+    printf("a area do circulo é: %4f", area);
+
+}
