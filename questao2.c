cf1b7448e792e11014871fb8e0683836e42c9aee
thays <thaysfmcandido14@gmail.com>
2026-04-02 21:08:58 -0300

    Add files via upload

---
+++ questao2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    printf("digite um número inteiro: ")
+    scanf("&d", &x);
+    printf("seu sucessor é: %d", x +1);
+    printf("seu antecessor é: %d", x -1);
+
+}
+
