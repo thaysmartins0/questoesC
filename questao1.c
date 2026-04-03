6b216fbdb523c83b8e65272f3fe9916024f72d58
thays <thaysfmcandido14@gmail.com>
2026-04-02 21:50:51 -0300

    Add multiplication program in questao1.c

---
+++ questao1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+int a, b;
+    printf("digite um numero inteiro: \n");
+    scanf("%d", &a);
+    printf("digite outro numero inteiro: \n");
+    scanf("%d", &b);
+    printf("o resultado da multiplicacao desses numeros e: %d", a*b);
+}
