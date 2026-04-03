699ce0378bf2ac6b8b512e993045ab20635c6fed
thays <thaysfmcandido14@gmail.com>
2026-04-02 21:58:20 -0300

    Update questao12.c

---
+++ questao12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+float media1, media2, somamedia, mediaDasMedias;
+    media1=(7+8+9)/3;
+    media2=(4+5+6)/3;
+    somamedia= media1+media2;
+    mediaDasMedias=somamedia/2;
+    printf("a media dos numeros 7, 8 e 9 e: %.2f\n", media1);
+    printf("a media de 4, 5 e 6 e: %.2f\n", media2);
+    printf("a soma das medidas e: %.2f\n", somamedia);
+    printf("a media das medidas e: %.2f\n", mediaDasMedias);
+
+}
