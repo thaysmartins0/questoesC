d2520fb9719431c7349fa60962be3f3b7d279250
thays <thaysfmcandido14@gmail.com>
2026-04-27 13:09:34 -0300

    Create questao15.c

---
+++ questao15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    //questao15
+    int a;
+    printf("digite um numero inteiro: \n");
+    scanf("%d", &a);
+    if(a%2 ==0){
+        printf("seu numero e par");
+    } else {
+        printf("seu numero e impar");
+    }
+}
