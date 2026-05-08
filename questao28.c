62389248ce0a75443f159695e7780f0db26c0f6b
thays <thaysfmcandido14@gmail.com>
2026-05-08 11:42:24 -0300

    Create questao28.c
    Implement a program to calculate the sum of numbers from 1 to 100.

---
+++ questao28.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int som;
+    for (i=0; i<=100; i++){
+        som=(1+100)*(100/2);
+        if(i==100){
+        printf("%d\n", som);
+    }
+    }
+}
